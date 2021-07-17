a3f1db4d829a86f0c6b7a9130708498838fb754c
26huitailang <26huitailang@gmail.com>
2021-07-17 23:07:37 +0800

    todo: code

--- README.md
+++ README.md
@@ -12,11 +12,20 @@ pip install sqlalchemy
 
 3.32.0 SQLITE_MAX_VARIABLE_NUMBER defaults to 32766, the version less than this is 999.
 
-check version or use ``
+check version
 ```
 sqlite3 -version
 
 > 3.28.0 2019-04-15 14:49:49 378230ae7f4b721c8b8d83c8ceb891449685cd23b1702a57841f1be40b5daapl
 ```
 
-This version
+```sql
+sqlite3 test.db
+
+> create table user (id integer primary key autoincrement, name text);
+```
+
+## solution
+
+1. text(IN_CONDITION)
+2. bindparam() with literal_execute=True
--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-from sqlalchemy import
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.engine import create_engine
+
+engine = create_engine("sqlite:///test.db", echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(30))
+
+    def __repr__(self):
+        return "<User %s>" % self.name
+
+
+def create_test_data(num=2000):
+    for i in range(num):
+        user = User(name="user_%s" % i)
+        session.add(user)
+    session.commit()
+
+
+if __name__ == "__main__":
+    # Base.metadata.create_all(engine, checkfirst=True)
+
+    # first create some test data
+    # create_test_data()
+
+    uids = ",".join([str(x) for x in range(400000)])
+    from sqlalchemy.sql import text
+
+    uids_in = text(f"user.id in ({uids})")
+    print(session.query(User).filter(uids_in).all())
+
+    from sqlalchemy import bindparam
+
+    # print(
+    #     session.query(User)
+    #     .filter(User.id.in_(bindparam("x", range(400000), expanding=True, literal_execute=True)))
+    #     .all()
+    # )
